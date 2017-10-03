a950642a7a40bbe73a5f3be8a3e270701640996c
Diogo Cardoso <diogocardoso92@outlook.com>
2017-10-04 00:02:07 +0100

    Lint

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,44 +1,44 @@
 module.exports = {
 
-  development: {
-    client: 'postgresql',
-    connection: {
-      host: '127.0.0.1',
-      user: 'postgres',
-      password: 'skillz21',
-      database: 'tvshows',
+    development: {
+        client: 'postgresql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'postgres',
+            password: 'skillz21',
+            database: 'tvshows',
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            directory: `${__dirname}/migrations`,
+        },
+        seeds: {
+            directory: `${__dirname}/migrations/seeds`,
+        },
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: `${__dirname}/migrations`,
-    },
-    seeds: {
-      directory: `${__dirname}/seeds`,
-    },
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      host: '164.132.107.157',
-      port: '5432',
-      user: 'postgres',
-      password: 'skillz21',
-      database: 'tvshows',
+    production: {
+        client: 'postgresql',
+        connection: {
+            host: '164.132.107.157',
+            port: '5432',
+            user: 'postgres',
+            password: 'skillz21',
+            database: 'tvshows',
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            directory: `${__dirname}/migrations`,
+        },
+        seeds: {
+            directory: `${__dirname}/migrations/seeds`,
+        },
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: `${__dirname}/migrations`,
-    },
-    seeds: {
-      directory: `${__dirname}/seeds`,
-    },
-  },
 
 };
