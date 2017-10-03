64de742ec10a73e2a4304ca3c942b51206077d67
Diogo Cardoso <tuniq@live.com.pt>
2017-10-04 00:01:56 +0100

    Refactored migrations and seeds

--- server/db/migrations/02-tvshows.js
+++ server/db/migrations/02-tvshows.js
@@ -1,8 +1,10 @@
 exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('tvshows', (table) => {
   table.increments('id');
   table.string('name').unique().notNullable();
-  table.string('description').notNullable();
-  table.enu('status', ['running', 'ended']);
+  table.text('description').notNullable();
+  table.enu('status', ['running', 'ended', 'canceled']);
+  table.string('imdb').unique().notNullable();
+  table.string('premiered').notNullable();
   table.timestamp('createdAt').defaultTo(knex.raw('now()'));
   table.timestamp('updatedAt').defaultTo(knex.raw('now()'));
 });
--- server/db/migrations/seeds/02-tvshows.js
+++ server/db/migrations/seeds/02-tvshows.js
@@ -5,15 +5,21 @@ exports.seed = function (knex, Promise) {
          name: 'The Blacklist',
          description: 'Raymond "Red" Reddington, one of the FBIs most wanted fugitives, surrenders in person at FBI Headquarters in Washington, D.C. He claims that he and the FBI have the same interests: bringing down dangerous criminals and terrorists. In the last two decades, he has made a list of criminals and terrorists that matter the most but the FBI cannot find because it does not know they exist. Reddington calls this "The Blacklist". Reddington will co-operate, but insists that he will speak only to Elizabeth Keen, a rookie FBI profiler.',
          status: 'running',
+         imdb: 'tt2741602',
+         premiered: 'Sep 2013',
        }))
        .then(() => knex('tvshows').insert({
          name: 'Silicon Valley',
          description: 'In the high-tech gold rush of modern Silicon Valley, the people most qualified to succeed are the least capable of handling success. A comedy partially inspired by Mike Judge s own experiences as a Silicon Valley engineer in the late 1980s.',
          status: 'running',
+         imdb: 'tt2575988',
+         premiered: 'Apr 2014',
        }))
        .then(() => knex('tvshows').insert({
          name: 'Better Call Saul',
          description: 'We meet him when the man who will become Saul Goodman is known as Jimmy McGill, a small-time lawyer searching for his destiny and, more immediately, hustling to make ends meet. Working alongside, and, often, against Jimmy, is "fixer" Mike Erhmantraut. The series tracks Jimmys transformation into the man who puts "criminal" in "criminal lawyer".',
          status: 'running',
+         imdb: 'tt3032476',
+         premiered: 'Feb 2015',
        }));
 };
