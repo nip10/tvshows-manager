a45ca6d3ae055d72e2de614f3cb17389096403d5
Diogo Cardoso <diogocardoso92@outlook.com>
2017-10-04 00:03:31 +0100

    Linted

--- server/server.js
+++ server/server.js
@@ -1,49 +1,47 @@
-require('dotenv').config();
-
-import app from './app';
 import http from 'http';
+import app from './app';
+
+function normalizePort(val) {
+    const p = parseInt(val, 10);
+    if (Number.isNaN(p)) {
+        return val;
+    }
+    if (p >= 0) {
+        return p;
+    }
+    return false;
+}
 
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-}
-
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
-  switch (error.code) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+    switch (error.code) {
     case 'EACCES':
-      console.error(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
+        console.error(`${bind} requires elevated privileges`);
+        process.exit(1);
+        break;
     case 'EADDRINUSE':
-      console.error(`${bind} is already in use`);
-      process.exit(1);
-      break;
+        console.error(`${bind} is already in use`);
+        process.exit(1);
+        break;
     default:
-      throw error;
-  }
+        throw error;
+    }
 }
 
 function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
-  console.log(`Listening on ${bind}`);
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+    console.log(`Listening on ${bind}`);
 }
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
