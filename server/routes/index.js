9f228fdd29461d3304017322661d7ebe6bcfadd7
Diogo Cardoso <diogocardoso92@outlook.com>
2017-10-04 00:04:20 +0100

    Linted

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Tv-shows Manager',
-  });
+    res.render('index', {
+        title: 'Tv-shows Manager',
+    });
 });
 
 module.exports = router;
