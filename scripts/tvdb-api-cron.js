70a4123892e8243a7b96c2b8caa800b276495bef
Diogo Cardoso <diogocardoso92@outlook.com>
2017-10-11 12:09:45 +0100

    Add cronjob to refresh thetvdb api token
    The API returns a JWT token when we POST to login. This token is only valid for 24h so I created this cronjob to refresh the token automatically every 23h

---
+++ scripts/tvdb-api-cron.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable quote-props */
+/* eslint-disable no-console */
+
+const { CronJob } = require('cron');
+const rp = require('request-promise');
+
+console.log('[CHILD] [0] Child process started!');
+
+// let oldToken = null;
+
+const job = new CronJob(
+    // Runs every day at midnight
+    '0 0 */23 * * *', // sec min hour dayofmonth month dayofweek
+    (() => {
+        // Job to run
+        console.log('[CHILD] [1] Cronjob triggered. Requesting the oldToken from the parent process.');
+        // Send msg to the "parent" process to request the old token
+        process.send('oldToken');
+        // Set the oldToken received from the "parent" process
+        process.once('message', (msg) => {
+            console.log('[CHILD] [3] Received oldToken from the parent process: ', msg);
+            const oldToken = msg;
+            // make the request for the new token
+            if (oldToken) {
+                // request options
+                const options = {
+                    uri: 'https://api.thetvdb.com/refresh_token',
+                    headers: {
+                        'Authorization': `Bearer ${oldToken}`,
+                    },
+                    json: true,
+                };
+                rp(options)
+                    .then((res) => {
+                        console.log('[CHILD] [4] New token received: ', res.token);
+                        // send the new token to the "parent" process
+                        process.send(res.token);
+                    })
+                    .catch((err) => {
+                        console.log('[CHILD] [4(E)] Error getting new token.');
+                        console.log('[CHILD] [4(E)] Error message: ', err.message);
+                        console.log('[CHILD] [4(E)] Auth header: ', err.options.headers);
+                    });
+            } else {
+                console.log(`[CHILD] [4(E2)] Didn't make the request because the oldToken is ${oldToken}`);
+            }
+        });
+    }), (() => {
+        // Job stopped
+        console.error('Job stopped.');
+    }),
+    true, // Start the job right now
+);
