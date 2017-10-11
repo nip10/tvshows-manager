b4ee1b40a8a78e315a2de6bd1cda7649d3788e6c
Diogo Cardoso <tuniq@live.com.pt>
2017-10-11 12:14:30 +0100

    Implement calendar model in a dedicated route

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import pick from 'lodash/pick';
+import Calendar from '../models/calendar';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('calendar', {
-    title: 'Tv-shows Manager',
-  });
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const cal = new Calendar(month, year);
+    cal.buildCalendar();
+    res.render('calendar', {
+        title: 'Tv-shows Manager',
+        calendar: pick(cal, ['daysFromPreviousMonth', 'daysFromNextMonth', 'calendarData']),
+    });
 });
 
 module.exports = router;
