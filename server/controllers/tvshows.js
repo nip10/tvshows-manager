e913f21247a80ea8fdf00b6d8166f237a16eba2d
Diogo Cardoso <diogocardoso92@outlook.com>
2017-11-02 23:52:10 +0000

    Add several functions to tvshows controller
    Functions added:
    getInfo: fetch information about tvshow
    getImage: fetch graphics related to a tvshow
    getNumSeasons: fetch number of seasons of a tvshow
    getEpisodesFromSeason: fetch episodes from a specific season
    getEpisodesFromUser: fetch episodes from tvshows that a user is following
    addShowToDb: add a tvshow to the db
    isShowOnDb: check if a tvshow is on the db
    addShowToUser: add a tvshow to the user's following list
    getEpisodesFromDb: get tvshow episodes from the db
    getTvShowInfoFromDb: get tvshow information from the db

--- server/controllers/tvshows.js
+++ server/controllers/tvshows.js
@@ -3,10 +3,15 @@
 import path from 'path';
 import rp from 'request-promise';
 import cp from 'child_process';
+import knex from '../db/connection';
 
 const { THETVDB_API_KEY } = process.env;
 const THETVDB_API_AUTH_LOGIN = 'https://api.thetvdb.com/login';
 const THETVDB_API_SEARCH = 'https://api.thetvdb.com/search/series';
+const THETVDB_API_INFO = seriesId => `https://api.thetvdb.com/series/${seriesId}`;
+const THETVDB_API_IMAGES = seriesId => `https://api.thetvdb.com/series/${seriesId}/images/query`;
+const THETVDB_API_EPISODES_QUERY = seriesId => `https://api.thetvdb.com/series/${seriesId}/episodes/query`;
+const THETVDB_API_SEASON = seriesId => `https://api.thetvdb.com/series/${seriesId}/episodes/summary`;
 
 const TvShows = {
     apiToken: null,
@@ -14,6 +19,9 @@ const TvShows = {
         // get "initial" token from the thetvdb api
         console.log('Requesting token from TheTvDb API');
         return this.getToken().then((newToken) => {
+            // if (!newToken || newToken.length === 0) {
+            //     throw Error('Token received is invalid.');
+            // }
             this.apiToken = newToken;
             console.log(`Token received: ${this.apiToken}`);
         }).then(() => {
@@ -43,7 +51,7 @@ const TvShows = {
             console.log('[PARENT] Child process has been terminated.');
         });
     },
-    getToken() {
+    async getToken() {
         const requestOptions = {
             method: 'POST',
             uri: THETVDB_API_AUTH_LOGIN,
@@ -52,9 +60,15 @@ const TvShows = {
             },
             json: true,
         };
-        return rp(requestOptions).then(data => data.token).catch(e => console.log(e));
+        try {
+            const { token } = await rp(requestOptions);
+            return token;
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
     },
-    search(tvshow) {
+    async search(tvshow) {
         const requestOptions = {
             method: 'GET',
             uri: THETVDB_API_SEARCH,
@@ -67,10 +81,255 @@ const TvShows = {
             },
             json: true,
         };
-        return rp(requestOptions).then(res => res.data.map(tvshows => ({ id: tvshows.id, seriesName: tvshows.seriesName }))).catch(e => console.log(e));
+        try {
+            const { data } = await rp(requestOptions);
+            return data.map(tvshows => ({
+                id: tvshows.id,
+                seriesName: tvshows.seriesName,
+            }));
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    async getInfo(tvshowId) {
+        const requestOptions = {
+            method: 'GET',
+            uri: THETVDB_API_INFO(tvshowId),
+            headers: {
+                'Accept-Language': 'en',
+                'Authorization': `Bearer ${this.apiToken}`,
+            },
+            json: true,
+        };
+        try {
+            const { data } = await rp(requestOptions);
+            return {
+                name: data.seriesName,
+                description: data.overview,
+                status: (data.status === 'Continuing') ? 'Running' : data.status,
+                imdb: data.imdbId,
+                thetvdb: data.id,
+                genre: data.genre,
+                premiered: data.firstAired,
+                network: data.network,
+                airdate: `${data.airsDayOfWeek} at ${data.airsTime}`,
+                tvrating: data.rating,
+                images: [data.banner],
+            };
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    async getImage(tvshowId, imageType) {
+        const requestOptions = {
+            method: 'GET',
+            uri: THETVDB_API_IMAGES(tvshowId),
+            headers: {
+                'Accept-Language': 'en',
+                'Authorization': `Bearer ${this.apiToken}`,
+            },
+            qs: {
+                keyType: imageType,
+            },
+            json: true,
+        };
+        try {
+            const { data } = await rp(requestOptions);
+            return data[0].fileName;
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    async getNumSeasons(tvshowId) {
+        const requestOptions = {
+            method: 'GET',
+            uri: THETVDB_API_SEASON(tvshowId),
+            headers: {
+                'Accept-Language': 'en',
+                'Authorization': `Bearer ${this.apiToken}`,
+            },
+            json: true,
+        };
+        try {
+            const { data } = await rp(requestOptions);
+            return data.airedSeasons.length - 1;
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    async getEpisodesFromSeason(tvshowId, season) {
+        const requestOptions = {
+            method: 'GET',
+            uri: THETVDB_API_EPISODES_QUERY(tvshowId),
+            headers: {
+                'Accept-Language': 'en',
+                'Authorization': `Bearer ${this.apiToken}`,
+            },
+            qs: {
+                airedSeason: season,
+            },
+            json: true,
+        };
+        try {
+            const { data } = await rp(requestOptions);
+            return data.map(episode => ({
+                num: episode.airedEpisodeNumber,
+                name: episode.episodeName,
+                airdate: episode.firstAired,
+                summary: episode.overview,
+            }));
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    async getEpisodesFromUser(userId, startInterval, endInterval) {
+        const getEpisodes = knex.select('tvshows.name', 'tvshows.thetvdb', 'episodes.title')
+            .select(knex.raw('to_char(episodes.season, \'fm00\') as "season"'))
+            .select(knex.raw('to_char(episodes.epnum, \'fm00\') as "epnum"'))
+            .select(knex.raw('to_char(episodes.airdate, \'YYYY-MM-DD\') as "airdate"'))
+            .from('episodes')
+            .join('usertv', 'usertv.tvshow_id', 'episodes.tvshow_id')
+            .join('tvshows', 'tvshows.thetvdb', 'episodes.tvshow_id')
+            .where('usertv.user_id', userId)
+            .andWhere(function () {
+                this.whereBetween('episodes.airdate', [startInterval, endInterval]);
+            });
+        try {
+            const episodes = await getEpisodes;
+            return episodes;
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    async addShowToDb(tvshowInfo) {
+        // 1. TvShow information
+        try {
+            // 1.2 Insert show information in the database
+            const insertShow = await knex('tvshows').insert(tvshowInfo);
+            if (insertShow.rowCount === 1) {
+                console.log(`Successfully added tvshow id ${tvshowInfo.thetvdb} to the db.`);
+            } else {
+                throw new Error(`Error inserting tvshow id ${tvshowInfo.thetvdb} to the db.`);
+            }
+        } catch (e) {
+            console.log(`Error inserting tvshow id ${tvshowInfo.thetvdb} to the db. Error details: ${e}`);
+            return;
+            // TODO: Check if this stops the function from running
+            // Force adding a show that already exists in the database
+        }
+        // 2. Episodes information
+        // 2.1 Fetch all episodes
+        const tvshowId = tvshowInfo.thetvdb;
+        const episodes = [];
+        /**
+         * Make requests to paginated api endpoints.
+         *
+         * @param {string} apiToken TheTVDb apiToken
+         * @param {number} [page=1] Page to fetch (defaults to 1)
+         * @returns {}
+         */
+        async function requestPaginated(apiToken, page = 1) {
+            console.log(`Fetching episodes from tvshow id ${tvshowId}`);
+            console.log(`Page ${page}`);
+            const requestOptions = {
+                method: 'GET',
+                uri: THETVDB_API_EPISODES_QUERY(tvshowId),
+                headers: {
+                    'Accept-Language': 'en',
+                    'Authorization': `Bearer ${apiToken}`,
+                },
+                qs: {
+                    page,
+                },
+                json: true,
+            };
+            try {
+                const res = await rp(requestOptions);
+                const filteredEpisodes = res.data.map(episode => ({
+                    tvshow_id: tvshowId,
+                    season: episode.airedSeason,
+                    epnum: episode.airedEpisodeNumber,
+                    title: episode.episodeName,
+                    description: episode.overview,
+                    airdate: episode.firstAired || null,
+                }));
+                console.log(`Page ${page} received.`);
+                Array.prototype.push.apply(episodes, filteredEpisodes);
+                console.log('Checking if there are more pages.');
+                if (res.links.next) {
+                    console.log('Next page available.');
+                    return requestPaginated(apiToken, res.links.next);
+                }
+                console.log('Finished.');
+                return true;
+            } catch (e) {
+                console.log(e);
+                return false;
+            }
+        }
+        try {
+            const requestEpisodes = await requestPaginated(this.apiToken);
+            // console.log(`Requested Episodes: ${requestEpisodes}`);
+            if (requestEpisodes) {
+                // 2.2 Insert episodes in the database
+                const insertEpisodes = await knex('episodes').insert(episodes);
+            } else {
+                throw Error('Error fetching episodes.');
+            }
+        } catch (e) {
+            console.log(`Error fetching episodes. Error details: ${e}`);
+        }
+    },
+    async isShowOnDb(tvshowId) {
+        const inner = knex.select(1).from('tvshows').where('thetvdb', tvshowId).limit(1);
+        try {
+            const isShowOnDb = await knex.raw(inner).wrap('select exists (', ')');
+            return isShowOnDb.rows[0].exists;
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    async addShowToUser(userId, tvshowId) {
+        try {
+            const addShowToUser = await knex('usertv').insert({ user_id: userId, tvshow_id: tvshowId });
+            return { tvshowId };
+        } catch (e) {
+            console.log(e);
+            return { error: e };
+        }
+    },
+    async getEpisodesFromDb(tvshowId, season) {
+        // this handles fetching the episodes when rendering tvshow/id and the select menu
+        try {
+            const getEpisodesFromDb = await knex('episodes').select().where('tvshow_id', tvshowId).andWhere('season', season);
+            return getEpisodesFromDb;
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    async getTvshowInfoFromDb(tvshowId) {
+        try {
+            const getTvshowInfoFromDb = await knex('tvshows').select().where('thetvdb', tvshowId);
+            return {
+                thetvdb: Number.parseInt(tvshowId, 10),
+                name: getTvshowInfoFromDb[0].name,
+                description: getTvshowInfoFromDb[0].description,
+                premiered: getTvshowInfoFromDb[0].premiered,
+                network: getTvshowInfoFromDb[0].network,
+                status: getTvshowInfoFromDb[0].status,
+                airdate: getTvshowInfoFromDb[0].airdate,
+                images: [...getTvshowInfoFromDb[0].images],
+                imdb: getTvshowInfoFromDb[0].imdb,
+                genre: [...getTvshowInfoFromDb[0].genre],
+                tvrating: getTvshowInfoFromDb[0].tvrating,
+            };
+        } catch (e) {
+            console.log(e);
+        }
     },
 };
 
 TvShows.init();
 
 module.exports = TvShows;
+
