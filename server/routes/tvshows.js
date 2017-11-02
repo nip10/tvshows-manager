0fed3a787f150d3adec0c339977050d3e503b188
Diogo Cardoso <diogocardoso92@outlook.com>
2017-11-02 23:58:53 +0000

    Add multiple tvshow routes
    Added route for tvshow page, fetch episodes from a tvshow and add a tvshow to a user's following list

--- server/routes/tvshows.js
+++ server/routes/tvshows.js
@@ -4,32 +4,92 @@ import TvShows from './../controllers/tvshows';
 
 const router = express.Router();
 
-router.get('/search/:name', (req, res) => {
-    const tvshow = req.params.name;
-    TvShows.search(tvshow).then(data => res.send(data)).catch(e => console.log(e.message));
+router.get('/search/:name', async (req, res) => {
+    const tvshowName = req.params.name;
+    try {
+        const searchResults = await TvShows.search(tvshowName);
+        res.send(searchResults);
+    } catch (e) {
+        console.log(e);
+    }
 });
 
-router.get('/:name', (req, res) => {
-    const tvshow = req.params.name;
+router.get('/:showid', async (req, res) => {
+    const { showid } = req.params;
+    // Check if the show is already on the database
+    const isShowOnDb = await TvShows.isShowOnDb(showid);
+    const latestSeason = await TvShows.getNumSeasons(showid);
+    let tvshowInfo;
+    let episodes;
+    if (isShowOnDb) {
+        // Get show information and episodes from the database
+        console.log(`tvshow id ${showid} is on the db`);
+        try {
+            tvshowInfo = await TvShows.getTvshowInfoFromDb(showid);
+            console.log('tvshowinfo from db');
+            console.log(JSON.stringify(tvshowInfo, null, 4));
+            episodes = await TvShows.getEpisodesFromDb(showid, latestSeason);
+            // console.log(JSON.stringify(episodes[0], null, 4));
+        } catch (e) {
+            console.log(e);
+        }
+    } else {
+        // Get show information and episodes from the thetvdb api
+        try {
+            console.log(`tvshow id ${showid} is not on the db`);
+            const tvshowData = await Promise.all([TvShows.getInfo(showid), TvShows.getImage(showid, 'poster'), TvShows.getEpisodesFromSeason(showid, latestSeason)]);
+            // console.log('tvshowData[0]');
+            // console.log(JSON.stringify(tvshowData[0], null, 4));
+            // console.log('tvshowData[1]');
+            // console.log(JSON.stringify(tvshowData[1], null, 4));
+            tvshowInfo = Object.assign(tvshowData[0], { images: [tvshowData[0].images[0], tvshowData[1]] });
+            console.log('tvshowinfo from api');
+            console.log(JSON.stringify(tvshowInfo, null, 4));
+            episodes = tvshowData[2];
+            // TODO: spawn child to take care of this ?
+            TvShows.addShowToDb(tvshowInfo);
+        } catch (e) {
+            console.log(e);
+        }
+    }
     res.render('tvshow', {
         title: 'Tv-shows Manager',
-        name: 'The Blacklist',
-        banner: 'https://www.thetvdb.com/banners/graphical/266189-g20.jpg',
-        poster: 'https://www.thetvdb.com/banners/posters/266189-13.jpg',
+        name: tvshowInfo.name,
+        banner: `https://www.thetvdb.com/banners/${tvshowInfo.images[0]}`,
+        poster: `https://www.thetvdb.com/banners/${tvshowInfo.images[1]}`,
+        description: tvshowInfo.description,
+        premiered: tvshowInfo.premiered,
+        network: tvshowInfo.network,
+        status: tvshowInfo.status,
+        airdate: tvshowInfo.airdate,
+        season: latestSeason,
+        episodes,
     });
-    // TvShows.getInfo(tvshow).then((data) => {
-    //     res.render('tvshow', {
-    //         title: 'Tv-shows Manager',
-    //         name: 'The Blacklist',
-    //         banner: 'https://www.thetvdb.com/banners/graphical/266189-g20.jpg',
-    //         poster: 'https://www.thetvdb.com/banners/posters/266189-13.jpg',
-    //     });
-    // }).catch((e) => {
-    //     res.render('error', {
-    //         message: 'Server Error. Try again later',
-    //         error: e,
-    //     });
-    // });
+});
+
+router.get('/:showid/episodes', async (req, res) => {
+    const { showid } = req.params;
+    const { season } = req.query;
+    try {
+        // TODO: this handles the season select menu
+        // at this point, the show should/is in the database
+        // so theres no need to use the thetvdb api.. just fetch the episodes from the db
+        const episodes = await TvShows.getEpisodesFromSeason(showid, season);
+        res.json({ episodes });
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+router.get('/:showid/add', async (req, res) => {
+    const { showid } = req.params;
+    const userId = req.user;
+    try {
+        const addShowToUser = await TvShows.addShowToUser(userId, showid);
+        res.json({ addShowToUser });
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 module.exports = router;
