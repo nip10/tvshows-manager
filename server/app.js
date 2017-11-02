a5a9bf73cee6b62529931cdf66593b9f7068b611
Diogo Cardoso <tuniq@live.com.pt>
2017-11-02 23:48:31 +0000

    Minor fix

--- server/app.js
+++ server/app.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res) => {
-    console.error(err);
+    console.log(err);
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
