4b3205a2bc856945c70b367d992c6f824ab5049f
Diogo Cardoso <diogocardoso92@outlook.com>
2017-11-02 23:57:16 +0000

    Implement calendar model

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,19 +1,32 @@
 import express from 'express';
 import pick from 'lodash/pick';
 import Calendar from '../models/calendar';
+import TvShows from '../controllers/tvshows';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const userId = req.user;
     const date = new Date();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const cal = new Calendar(month, year);
     cal.buildCalendar();
-    res.render('calendar', {
-        title: 'Tv-shows Manager',
-        calendar: pick(cal, ['daysFromPreviousMonth', 'daysFromNextMonth', 'calendarData']),
-    });
+    const startInterval = cal.calendarData[0].day;
+    const endInterval = cal.calendarData[cal.calendarData.length - 1].day;
+    try {
+        const episodes = await TvShows.getEpisodesFromUser(userId, startInterval, endInterval);
+        if (episodes) {
+            cal.addEpisodesToCalendar(episodes);
+        }
+        res.render('calendar', {
+            title: 'Tv-shows Manager',
+            calendar: pick(cal, ['daysFromPreviousMonth', 'daysFromNextMonth', 'calendarData']),
+        });
+    } catch (e) {
+        console.log(e);
+        res.send('error');
+    }
 });
 
 module.exports = router;
