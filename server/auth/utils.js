b0b08f75f37e440afb20fd498b7d4e11c905bd82
Diogo Cardoso <tuniq@live.com.pt>
2017-11-12 13:16:46 +0000

    Add authentication middleware
    The old isLoggedIn is now isLoggedInWithRedirect, which redirects to the homepage if a user is not logged in. The new one responds with unauthorized and a message, to be used in ajax requests that dont need redirection.

--- server/auth/utils.js
+++ server/auth/utils.js
@@ -4,13 +4,21 @@ import knex from '../db/connection';
 
 module.exports = {
     comparePass: (userPassword, dbPassword) => bcrypt.compareSync(userPassword, dbPassword),
+    isLoggedInWithRedirect: (req, res, next) => {
+        // if user is authenticated in the session, carry on
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        // if he isnt, redirect him to the home page
+        return res.redirect('/');
+    },
     isLoggedIn: (req, res, next) => {
         // if user is authenticated in the session, carry on
         if (req.isAuthenticated()) {
             return next();
         }
-        // if they aren't redirect them to the home page
-        return res.redirect('/');
+        // if he isnt, send not autorized
+        return res.status(401).json({ error: 'Please login/signup first.' });
     },
     createUser: (req) => {
         const salt = bcrypt.genSaltSync();
