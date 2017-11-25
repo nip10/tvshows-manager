5b15a5a8003c818545424ac27afc0b5b36ab8943
Diogo Cardoso <tuniq@live.com.pt>
2017-11-25 13:07:49 +0000

    Add documentation and add conditional to child process
    Added documentation for all functions.
    Child process doesnt start if env is development.

--- server/controllers/tvshows.js
+++ server/controllers/tvshows.js
@@ -6,7 +6,7 @@ import rp from 'request-promise';
 import cp from 'child_process';
 import knex from '../db/connection';
 
-const { THETVDB_API_KEY } = process.env;
+const { THETVDB_API_KEY, NODE_ENV } = process.env;
 const THETVDB_API_AUTH_LOGIN = 'https://api.thetvdb.com/login';
 const THETVDB_API_SEARCH = 'https://api.thetvdb.com/search/series';
 const THETVDB_API_INFO = seriesId => `https://api.thetvdb.com/series/${seriesId}`;
@@ -15,21 +15,34 @@ const THETVDB_API_EPISODES_QUERY = seriesId => `https://api.thetvdb.com/series/$
 const THETVDB_API_SEASON = seriesId => `https://api.thetvdb.com/series/${seriesId}/episodes/summary`;
 
 const TvShows = {
-    apiToken: null,
+    /**
+     * Initialize connection to the TheTVDb API and
+     * start child process to renew the token
+     * every X hours
+     */
     init() {
         // get "initial" token from the thetvdb api
         console.log('Requesting token from TheTvDb API');
-        return this.getToken().then((newToken) => {
-            if (!newToken || newToken.length === 0) {
-                throw Error('Token received is invalid.');
-            }
-            this.apiToken = newToken;
-            console.log(`Token received: ${this.apiToken}`);
-        }).then(() => {
-            // start child process to renew the token when needed
-            this.startChildProcess();
-        }).catch(e => console.log(e));
+        return this.getToken()
+            .then((newToken) => {
+                if (!newToken || newToken.length === 0) {
+                    throw new Error('Token received is invalid.');
+                }
+                this.apiToken = newToken;
+                console.log(`Token received: ${this.apiToken}`);
+            }).then(() => {
+                if (NODE_ENV !== 'development') {
+                    // start child process to renew the token when needed
+                    this.startChildProcess();
+                }
+            }).catch(e => console.log(e));
     },
+    /**
+     * Start child process that renews the TheTVDB api token
+     *
+     * This child process is nothing more than a cronjob
+     * that runs every X hours and sends the token back here
+     */
     startChildProcess() {
         // create child process using fork
         const child = cp.fork(path.join(__dirname, '../../scripts/tvdb-api-cron'));
@@ -52,6 +65,11 @@ const TvShows = {
             console.log('[PARENT] Child process has been terminated.');
         });
     },
+    /**
+     * Get TheTVDb api token
+     *
+     * @returns {string} TheTVDb api token
+     */
     async getToken() {
         const requestOptions = {
             method: 'POST',
@@ -69,6 +87,12 @@ const TvShows = {
             return null;
         }
     },
+    /**
+     * Search for a tvshow
+     *
+     * @param {string} tvshowName - tvshow name
+     * @returns {{id: Number, seriesName: String}[]} - array of tvshow objects
+     */
     async search(tvshowName) {
         const requestOptions = {
             method: 'GET',
@@ -92,6 +116,12 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Get information about a tvshow
+     *
+     * @param {number} tvshowId - tvshow id
+     * @returns {{name: string, overview: string, status: string, imdb: string, thetvdb: number, genre: string[], premiered: string, network: string, airdate: string, tvrating: string, images: string[]}} Information about the show
+     */
     async getInfo(tvshowId) {
         const requestOptions = {
             method: 'GET',
@@ -121,6 +151,13 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Get artwork of a tvshow
+     *
+     * @param {number} tvshowId tvshow id
+     * @param {string} imageType artwork type (poster, banner)
+     * @returns {string} url for the first artwork of specified type
+     */
     async getImage(tvshowId, imageType) {
         const requestOptions = {
             method: 'GET',
@@ -141,6 +178,16 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Get number of seasons of a tvshow
+     *
+     * The api has an array of "airedSeasons" that contains all the seasons for a tvshow.
+     * This array contains a season 0, which is where the special episodes are placed.
+     * This is why we need to subtract 1 from the length.
+     *
+     * @param {number} tvshowId tvshow id
+     * @returns {number} number of seasons
+     */
     async getNumSeasons(tvshowId) {
         const requestOptions = {
             method: 'GET',
@@ -158,6 +205,13 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Get all tvshow episodes from a particular season
+     *
+     * @param {number} tvshowId tvshow id
+     * @param {number} season season
+     * @returns {{num: number, name: string, airdate: string, overview: string}[]} Tvshow episodes from a particular season
+     */
     async getEpisodesFromSeason(tvshowId, season) {
         const requestOptions = {
             method: 'GET',
@@ -183,6 +237,14 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Get episodes from all tvshows that a user is following, in a particular date interval
+     *
+     * @param {number} userId user id
+     * @param {date} startInterval
+     * @param {date} endInterval
+     * @returns {{}[]} Tvshow episodes that a user if following, in the specified date interval
+     */
     async getEpisodesFromUser(userId, startInterval, endInterval) {
         const getEpisodes = knex.select('tvshows.name', 'tvshows.thetvdb', 'episodes.title')
             .select(knex.raw('to_char(episodes.season, \'fm00\') as "season"'))
@@ -202,11 +264,17 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Add new tvshow to the database
+     *
+     * @param {{}} tvshowInfo Tvshow information
+     * @returns {Promise}
+     */
     async addShowToDb(tvshowInfo) {
         // 1. TvShow information
         try {
             // 1.2 Insert show information in the database
-            const insertShow = await knex('tvshows').insert(tvshowInfo);
+            await knex('tvshows').insert(tvshowInfo);
             console.log(`Successfully added tvshow id ${tvshowInfo.thetvdb} to the db.`);
         } catch (e) {
             console.log(`Error inserting tvshow id ${tvshowInfo.thetvdb} to the db. Error details: ${e}`);
@@ -221,7 +289,7 @@ const TvShows = {
          *
          * @param {string} apiToken TheTVDb apiToken
          * @param {number} [page=1] Page to fetch (defaults to 1)
-         * @returns {}
+         * @returns {boolean} Request status (true if successful)
          */
         async function requestPaginated(apiToken, page = 1) {
             console.log(`Fetching episodes from tvshow id ${tvshowId}`);
@@ -265,7 +333,7 @@ const TvShows = {
             const requestEpisodes = await requestPaginated(this.apiToken);
             if (requestEpisodes) {
                 // 2.2 Insert episodes in the database
-                const insertEpisodes = await knex('episodes').insert(episodes);
+                await knex('episodes').insert(episodes);
             } else {
                 throw Error('Error fetching episodes.');
             }
@@ -273,6 +341,12 @@ const TvShows = {
             console.log(`Error fetching episodes. Error details: ${e}`);
         }
     },
+    /**
+     * Check if a tvshow is on the database
+     *
+     * @param {number} tvshowId tvshow id
+     * @returns {boolean} tvshow is on the database
+     */
     async isShowOnDb(tvshowId) {
         const inner = knex.select(1).from('tvshows').where('thetvdb', tvshowId).limit(1);
         try {
@@ -282,6 +356,13 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Add tvshow to user
+     *
+     * @param {number} userId user id
+     * @param {number} tvshowId tvshow id
+     * @returns {{tvshowid: number}} tvshow id if successful
+     */
     async addShowToUser(userId, tvshowId) {
         try {
             const addShowToUser = await knex('usertv').insert({ user_id: userId, tvshow_id: tvshowId });
@@ -294,6 +375,13 @@ const TvShows = {
             return { error: e };
         }
     },
+    /**
+     * Remove tvshow from user
+     *
+     * @param {number} userId user id
+     * @param {number} tvshowId tvshow id
+     * @returns {{tvshowid: number}} tvshow id if successful
+     */
     async removeShowFromUser(userId, tvshowId) {
         try {
             const removeShowFromUser = await knex('usertv').where({ user_id: userId, tvshow_id: tvshowId }).del();
@@ -306,6 +394,13 @@ const TvShows = {
             return { error: e };
         }
     },
+    /**
+     * Get tvshow episodes from a particular season
+     *
+     * @param {number} tvshowId tvshow id
+     * @param {number} season season
+     * @returns {{}[]} Tvshow episodes from the specified season
+     */
     async getEpisodesFromDb(tvshowId, season) {
         try {
             const getEpisodesFromDb = await knex('episodes')
@@ -322,6 +417,12 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Get tvshow information from the database
+     *
+     * @param {number} tvshowId tvshow id
+     * @returns {{thetvdb: number, name: string, overview: string, premiered: string, network: string, status: string, airdate: string, images: string[], imdb: string, genre: string[], tvrating: string}} Tvshow information
+     */
     async getTvshowInfoFromDb(tvshowId) {
         try {
             const getTvshowInfoFromDb = await knex('tvshows').select().where('thetvdb', tvshowId);
@@ -342,6 +443,13 @@ const TvShows = {
             console.log(e);
         }
     },
+    /**
+     * Check if a user is following a particular tvshow
+     *
+     * @param {number} userId user id
+     * @param {number} tvshowId tvshow id
+     * @returns {boolean} true if the user is following the specified tvshow
+     */
     async isUserFollowingTvshow(userId, tvshowId) {
         const inner = knex.select(1).from('usertv').where({ user_id: userId, tvshow_id: tvshowId }).limit(1);
         try {
