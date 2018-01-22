fa0c2103118664a14607f00a0e8e5047a13cf31c
Diogo Cardoso <diogocardoso92@outlook.com>
2018-01-22 16:56:11 +0000

    Fix file paths

--- src/db/migrations/01-users.js
+++ src/db/migrations/01-users.js
@@ -1,13 +1,13 @@
 exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('users', (table) => {
-  table.increments('id');
-  table.string('username').unique().notNullable();
-  table.string('email').unique().notNullable();
-  table.string('password').notNullable();
-  table.boolean('admin').defaultTo(false);
-  table.string('resetpwtoken');
-  table.timestamp('resetpwexp');
-  table.timestamp('createdAt').defaultTo(knex.fn.now());
-  table.timestamp('updatedAt').defaultTo(knex.fn.now());
+    table.increments('id');
+    table.string('username').unique().notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.boolean('admin').defaultTo(false);
+    table.string('resetpwtoken');
+    table.timestamp('resetpwexp');
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+    table.timestamp('updatedAt').defaultTo(knex.fn.now());
 });
 
 exports.down = (knex, Promise) => knex.schema.dropTable('users');
