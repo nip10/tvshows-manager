af2d62f98a1ecbb1e23c29f1bf37b4cd155ea51f
Diogo Cardoso <tuniq@live.com.pt>
2018-01-23 02:53:28 +0000

    Add usereps table to the database
    This table will keep track of the watched/unwatched episodes

---
+++ src/db/migrations/06-usereps.js
@@ -0,0 +1,9 @@
+exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('usereps', (table) => {
+    table.increments('id');
+    table.integer('user_id').references('users.id');
+    table.integer('tvshow_id').references('tvshows.thetvdb');
+    table.integer('ep_id').references('episodes.id');
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+  });
+  
+  exports.down = (knex, Promise) => knex.schema.dropTable('usereps');
