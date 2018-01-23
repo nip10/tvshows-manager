6124da065c8ab51a0c93e967d0d541bd491dc102
Diogo Cardoso <diogocardoso92@outlook.com>
2018-01-23 02:53:03 +0000

    Lint

--- src/db/migrations/02-tvshows.js
+++ src/db/migrations/02-tvshows.js
@@ -1,18 +1,19 @@
 exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('tvshows', (table) => {
-  table.increments('id');
-  table.string('name').unique().notNullable();
-  table.text('overview');
-  table.enu('status', ['Running', 'Ended', 'Canceled']);
-  table.string('imdb').unique();
-  table.integer('thetvdb').unique();
-  table.specificType('genre', 'text[]');
-  table.string('premiered');
-  table.string('network');
-  table.string('airdate');
-  table.string('tvrating');
-  table.specificType('images', 'text[]');
-  table.timestamp('createdAt').defaultTo(knex.fn.now());
-  table.timestamp('updatedAt').defaultTo(knex.fn.now());
+    table.increments('id');
+    table.string('name').unique().notNullable();
+    table.text('overview');
+    table.enu('status', ['Running', 'Ended', 'Canceled']);
+    table.string('imdb').unique();
+    table.real('imdbrating').defaultTo(null);
+    table.integer('thetvdb').unique();
+    table.specificType('genre', 'text[]');
+    table.string('premiered');
+    table.string('network');
+    table.string('airdate');
+    table.string('tvrating');
+    table.specificType('images', 'text[]');
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+    table.timestamp('updatedAt').defaultTo(knex.fn.now());
 });
 
 exports.down = (knex, Promise) => knex.schema.dropTable('tvshows');
