f20d04ed6e838f27583eb557243bf1ac213a108e
Diogo Cardoso <tuniq@live.com.pt>
2018-01-23 02:53:03 +0000

    Lint

--- src/db/migrations/03-episodes.js
+++ src/db/migrations/03-episodes.js
@@ -1,12 +1,12 @@
 exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('episodes', (table) => {
-  table.increments('id');
-  table.integer('tvshow_id').references('tvshows.thetvdb');
-  table.integer('season').notNullable();
-  table.integer('epnum').notNullable();
-  table.string('title');
-  table.text('overview');
-  table.date('airdate');
-  table.timestamp('createdAt').defaultTo(knex.fn.now());
+    table.increments('id');
+    table.integer('tvshow_id').references('tvshows.thetvdb');
+    table.integer('season').notNullable();
+    table.integer('epnum').notNullable();
+    table.string('title');
+    table.text('overview');
+    table.date('airdate');
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
 });
 
 exports.down = (knex, Promise) => knex.schema.dropTable('episodes');
