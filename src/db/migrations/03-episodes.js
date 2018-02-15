6dc458bb1454f1054a228ecc54eeeb0272fa6392
Diogo Cardoso <tuniq@live.com.pt>
2018-02-15 00:55:31 +0000

    Remove createTableIfNotExists from migrations
    createTableIfNotExists method is not advised since there were some discovered some side-effects.

--- src/db/migrations/03-episodes.js
+++ src/db/migrations/03-episodes.js
@@ -1,12 +1,16 @@
-exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('episodes', (table) => {
-    table.increments('id');
-    table.integer('tvshow_id').references('tvshows.thetvdb');
-    table.integer('season').notNullable();
-    table.integer('epnum').notNullable();
-    table.string('title');
-    table.text('overview');
-    table.date('airdate');
-    table.timestamp('createdAt').defaultTo(knex.fn.now());
+exports.up = (knex, Promise) => knex.schema.hasTable('episodes', (exists) => {
+    if (!exists) {
+        return knex.schema.createTable('episodes', (table) => {
+            table.increments('id');
+            table.integer('tvshow_id').references('tvshows.thetvdb');
+            table.integer('season').notNullable();
+            table.integer('epnum').notNullable();
+            table.string('title');
+            table.text('overview');
+            table.date('airdate');
+            table.timestamp('createdAt').defaultTo(knex.fn.now());
+        });
+    }
 });
 
-exports.down = (knex, Promise) => knex.schema.dropTable('episodes');
+exports.down = (knex, Promise) => knex.schema.dropTableIfExists('episodes');
