2d8fdf88634129b42bfb13fc1b8c4aea14e61fce
Diogo Cardoso <diogocardoso92@outlook.com>
2018-02-16 17:45:15 +0000

    Fix eslint rules. Update gulp watch.
    When in dev, gulp will trigger a browsersync reload on pug file changes.

--- gulp-tasks/views.js
+++ gulp-tasks/views.js
@@ -1,9 +1,16 @@
 module.exports = function() {
   const gulp = require('gulp');
   const util = require('gulp-util');
+  const browserSync = require('browser-sync');
 
   const { config } = util.env.boilerplate;
   const viewsConfig = config.tasks.views;
+
+  const isWatching = ['serve', 'watch'].indexOf(process.argv[2]) >= 0;
+
+  if (isWatching) {
+    return browserSync.reload();
+  }
 
   return gulp
     .src(viewsConfig.source, { cwd: config.sourceRoot })
