e42b1cb2c8013cb00a1f94d549b032307ac7f56d
Diogo Cardoso <diogocardoso92@outlook.com>
2018-02-19 20:52:55 +0000

    Add email config and templates

---
+++ src/mail/mail.js
@@ -0,0 +1,52 @@
+import Email from 'email-templates';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { NODE_ENV, EMAIL_HOST, EMAIL_PORT, EMAIL_AUTH_USER, EMAIL_AUTH_PASSWORD, HOSTNAME } = process.env;
+const isDev = NODE_ENV === 'development';
+
+const mail = {
+  async sendEmail(to, template, locals) {
+    const email = new Email({
+      message: {
+        from: `${EMAIL_AUTH_USER}@${HOSTNAME}`,
+        to,
+      },
+      send: !isDev,
+      transport: {
+        host: EMAIL_HOST,
+        port: EMAIL_PORT,
+        secure: false,
+        auth: {
+          user: EMAIL_AUTH_USER,
+          pass: EMAIL_AUTH_PASSWORD,
+        },
+        debug: isDev,
+        requireTLS: true,
+      },
+      views: {
+        root: path.join(__dirname, 'templates'),
+      },
+      juiceResources: {
+        webResources: {
+          relativeTo: path.join(__dirname, 'templates', template),
+        },
+      },
+    });
+    try {
+      await email.send({
+        template,
+        message: { to },
+        locals,
+      });
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  },
+};
+
+module.exports = mail;
