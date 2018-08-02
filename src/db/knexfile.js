cd960be18e08972ac657fad61106269bfd9878f8
Diogo Cardoso <diogocardoso92@outlook.com>
2018-08-02 03:54:19 +0100

    Fix dotenv config path

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
+const path = require('path');
 
-// dotenv.config({ path: '../../.env' });
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const {
   DATABASE_HOST,
