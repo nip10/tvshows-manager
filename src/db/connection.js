b91acece36c627327eca26fb090a5f1c0c002a4a
Diogo Cardoso <tuniq@live.com.pt>
2018-08-02 03:54:19 +0100

    Fix dotenv config path

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
+const path = require('path');
 
-// dotenv.config({ path: '../../.env' });
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const environment = process.env.NODE_ENV;
 const config = require('./knexfile.js')[environment];
