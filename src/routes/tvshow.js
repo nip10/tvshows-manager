5c55392e93bb88959ca67a5341564d96dbcc667e
Diogo Cardoso <tuniq@live.com.pt>
2018-08-08 17:33:02 +0100

    Separate add/remove tvshow routes

--- src/routes/tvshow.js
+++ src/routes/tvshow.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { search, searchFull, getEpisodes, getData } from '../controllers/tvshow';
-import { setFollowingTvshow, setEpisodeWatchedStatus, setSeasonWatched } from '../controllers/user';
+import { addTvshow, removeTvshow, setEpisodeWatchedStatus, setSeasonWatched } from '../controllers/user';
 import { isLoggedIn } from '../controllers/auth';
 
 const router = express.Router();
@@ -9,7 +9,8 @@ router.get('/search/:tvshowName', search);
 router.get('/search_full/:tvshowName', searchFull);
 router.get('/:tvshowId', getData);
 router.get('/:tvshowId/episodes', getEpisodes);
-router.post('/:tvshowId', isLoggedIn, setFollowingTvshow);
+router.post('/:tvshowId/add', isLoggedIn, addTvshow);
+router.post('/:tvshowId/remove', isLoggedIn, removeTvshow);
 router.post('/:tvshowId/ep', isLoggedIn, setEpisodeWatchedStatus);
 router.post('/:tvshowId/s', isLoggedIn, setSeasonWatched);
 
