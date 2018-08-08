a2ad027bd0a333da2273300cd8a2e3267efbd4cb
Diogo Cardoso <tuniq@live.com.pt>
2018-08-08 15:54:21 +0100

    Minor fix's

--- src/controllers/calendar.js
+++ src/controllers/calendar.js
@@ -19,18 +19,14 @@ const calendarController = {
    * @returns {undefined}
    */
   async getCalendar(req, res) {
-    const userId = Number.parseInt(_.get(req, 'user'), 10);
-    if (!_.isFinite(userId)) {
-      return res.status(500).render('error', {
-        error: ERROR.AUTH.INVALID_ID,
-      });
-    }
-    let month = Number.parseInt(_.get(req, 'params.month'), 10);
-    let year = Number.parseInt(_.get(req, 'params.year'), 10);
+    const userId = Number.parseInt(req.user, 10);
+    let month = Number.parseInt(req.params.month, 10);
+    let year = Number.parseInt(req.params.year, 10);
     if (!_.isFinite(month) || !_.isFinite(year)) {
+      // If month/year is not defined, default to current month/year
       const date = new Date();
-      month = Number.parseInt(date.getMonth() + 1, 10);
-      year = Number.parseInt(date.getFullYear(), 10);
+      month = Number.parseInt(date.getMonth() + 1, 10, 10);
+      year = Number.parseInt(date.getFullYear(), 10, 10);
     }
     const calendar = new Calendar(month, year);
     calendar.buildCalendar();
@@ -38,17 +34,17 @@ const calendarController = {
     const endInterval = calendar.calendarData[calendar.calendarData.length - 1].day;
     try {
       const episodes = await User.getEpisodes(userId, startInterval, endInterval);
-      if (!_.isNil(episodes) && !_.isEmpty(episodes)) {
-        const episodeIds = _.map(episodes, episode => episode.id);
-        const seasonFinaleEpisodes = await Tvshow.getSeasonFinaleEpisodes(episodeIds);
+      const episodeIds = _.map(episodes, episode => episode.id);
+      const seasonFinaleEpisodes = await Tvshow.getSeasonFinaleEpisodes(episodeIds);
+      if (!_.isNil(seasonFinaleEpisodes) && !_.isEmpty(seasonFinaleEpisodes)) {
         for (const ep of seasonFinaleEpisodes) {
           const epIndex = episodes.findIndex(element => element.id === ep.id);
           if (epIndex !== -1) {
             episodes[epIndex].isSeasonFinale = true;
           }
         }
-        calendar.addEpisodesToCalendar(episodes);
       }
+      calendar.addEpisodesToCalendar(episodes);
       return res.render('calendar', {
         calendar: _.pick(calendar, ['day', 'daysFromPreviousMonth', 'daysFromNextMonth', 'calendarData']),
         monthNavigation: {
@@ -62,7 +58,6 @@ const calendarController = {
         sidebarIndex: 'calendar',
       });
     } catch (e) {
-      console.log(e);
       return res.status(500).render('error', {
         error: ERROR.SERVER,
       });
